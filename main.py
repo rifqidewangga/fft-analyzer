649fdb25159e1148bc4ac6882571a7e0d17cc12c
rifqidewangga <rifdewangga@gmail.com>
2022-02-02 22:11:48 +0700

    Added open csv feature

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import filedialog as fd
 
 from matplotlib.widgets import SpanSelector
 
-from fft_tools import calculate_fft, np
+from fft_tools import calculate_fft, np, get_3axis_raw_data, generate_time_array
 from toolbar_controller import *
 
 TIME_DATA_SAMPLE = np.arange(0.0, 5.0, 0.001)
@@ -10,24 +10,26 @@ X_DATA_SAMPLE = np.sin(100 * np.pi * TIME_DATA_SAMPLE) + 0.5 * np.random.randn(l
 Y_DATA_SAMPLE = np.sin(350 * np.pi * TIME_DATA_SAMPLE) + 0.5 * np.random.randn(len(TIME_DATA_SAMPLE))
 Z_DATA_SAMPLE = np.sin(134 * np.pi * TIME_DATA_SAMPLE) + 0.5 * np.random.randn(len(TIME_DATA_SAMPLE))
 
-AXIS_LABELS = ('x', 'y', 'z')
+AXIS_KEYS = ('x', 'y', 'z')
 
-time_data = TIME_DATA_SAMPLE
-raw_data = [X_DATA_SAMPLE, Y_DATA_SAMPLE, Z_DATA_SAMPLE]
+MEASUREMENT_PERIOD = 0.001
+TIME_DATA = TIME_DATA_SAMPLE
+RAW_DATA_DICT = {AXIS_KEYS[0]: X_DATA_SAMPLE,
+                 AXIS_KEYS[1]: Y_DATA_SAMPLE,
+                 AXIS_KEYS[2]: Z_DATA_SAMPLE}
 
-fig = plt.figure('FFT Analyzer')
+FIG = plt.figure('FFT Analyzer')
 
-axes_raw = fig.add_subplot(211)
-axes_fft = fig.add_subplot(212)
+SUBPLOT_RAW = FIG.add_subplot(211)
+SUBPLOT_FFT = FIG.add_subplot(212)
 
-raw_lines_dict = {}
-fft_lines_dict = {}
+RAW_LINES_DICT = {}
+FFT_LINES_DICT = {}
 
 
 def select_file():
     filetypes = (
         ('csv files', '*.csv'),
-        ('All files', '*.*')
     )
 
     filename = fd.askopenfilename(
@@ -36,12 +38,28 @@ def select_file():
         filetypes=filetypes
     )
 
-    return filename
+    x, y, z = get_3axis_raw_data(filename, remove_dc=True)
+    new_raw_data = [x, y, z]
+    # prompt measurement period
+    measurement_period = 0.001
+
+    global TIME_DATA
+    TIME_DATA = generate_time_array(np.array(x), measurement_period=measurement_period)
+
+    updated_raw_data_dict = {}
+    for axis_key, data in zip(AXIS_KEYS, new_raw_data):
+        updated_raw_data_dict[axis_key] = np.array(data)
+
+    global RAW_DATA_DICT
+    RAW_DATA_DICT.update(updated_raw_data_dict)
+
+    update_raw_plot()
+    update_fft_plot(0, -1)
 
 
 def on_select(x_min, x_max):
-    ind_min, ind_max = np.searchsorted(time_data, (x_min, x_max))
-    ind_max = min(len(time_data) - 1, ind_max)
+    ind_min, ind_max = np.searchsorted(TIME_DATA, (x_min, x_max))
+    ind_max = min(len(TIME_DATA) - 1, ind_max)
 
     if ind_max == ind_min:
         return
@@ -49,45 +67,59 @@ def on_select(x_min, x_max):
     update_fft_plot(ind_min, ind_max)
 
 
+def update_raw_plot():
+    for line_key, data_key in zip(RAW_LINES_DICT, RAW_DATA_DICT):
+        line = RAW_LINES_DICT[line_key]
+        data = RAW_DATA_DICT[data_key]
+
+        line.set_data(TIME_DATA, data)
+
+    FIG.canvas.draw_idle()
+
+
 def update_fft_plot(ind_min, ind_max):
-    for key, data in zip(fft_lines_dict, raw_data):
+    for line_key, data_key in zip(FFT_LINES_DICT, RAW_DATA_DICT):
+        line = FFT_LINES_DICT[line_key]
+        data = RAW_DATA_DICT[data_key]
+
         selected_data = np.array(data[ind_min:ind_max])
         freq_trimmed, fft_trimmed = calculate_fft(selected_data)
-        line = fft_lines_dict[key]
         line.set_data(freq_trimmed, fft_trimmed)
 
-    fig.canvas.draw_idle()
+    FIG.canvas.draw_idle()
 
 
 def apply_raw_lines_style():
-    axes_raw.legend(loc='lower right')
+    SUBPLOT_RAW.legend(loc='lower right')
 
 
 def apply_fft_lines_style():
-    axes_fft.legend(loc='lower right')
+    SUBPLOT_FFT.legend(loc='lower right')
 
 
 def create_fft_lines():
-    for key, data, label in zip(AXIS_LABELS, raw_data, AXIS_LABELS):
+    for axis_key, data_key in zip(AXIS_KEYS, RAW_DATA_DICT):
+        data = RAW_DATA_DICT[data_key]
         res_freq, res_fft = calculate_fft(data)
-        line, = axes_fft.plot(res_freq, res_fft, linewidth=0.3, label=label)
-        fft_lines_dict[key] = line
+        line, = SUBPLOT_FFT.plot(res_freq, res_fft, linewidth=0.3, label=axis_key)
+        FFT_LINES_DICT[axis_key] = line
 
 
 def create_raw_lines():
-    for key, data, label in zip(AXIS_LABELS, raw_data, AXIS_LABELS):
-        line, = axes_raw.plot(time_data, data, linewidth=0.3, label=label, )
-        raw_lines_dict[key] = line
+    for axis_key, data_key in zip(AXIS_KEYS, RAW_DATA_DICT):
+        data = RAW_DATA_DICT[data_key]
+        line, = SUBPLOT_RAW.plot(TIME_DATA, data, linewidth=0.3, label=axis_key)
+        RAW_LINES_DICT[axis_key] = line
 
 
 def toggle_line_visibility(key):
-    raw_line = raw_lines_dict[key]
-    fft_line = fft_lines_dict[key]
+    raw_line = RAW_LINES_DICT[key]
+    fft_line = FFT_LINES_DICT[key]
 
     raw_line.set_visible(not raw_line.get_visible())
     fft_line.set_visible(not fft_line.get_visible())
 
-    fig.canvas.draw_idle()
+    FIG.canvas.draw_idle()
 
 
 def create_callback(key):
@@ -104,10 +136,10 @@ def main():
     create_fft_lines()
     apply_fft_lines_style()
 
-    span = SpanSelector(axes_raw, on_select, 'horizontal', useblit=True,
+    span = SpanSelector(SUBPLOT_RAW, on_select, 'horizontal', useblit=True,
                         props=dict(alpha=0.5, facecolor='red'))
 
-    c = ToolbarController(fig)
+    c = ToolbarController(FIG)
 
     c.set_callback(OpenFile.callback_key, select_file)
     c.set_callback(ToggleXVisibility.callback_key, create_callback('x'))
