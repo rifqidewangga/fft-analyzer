be1b7c002cfbb33d96aec74a1616a7a36bf29e1e
rifqidewangga <rifdewangga@gmail.com>
2022-02-13 15:42:35 +0700

    Added more documentation in dataset.py

--- app/dataset.py
+++ app/dataset.py
@@ -1,3 +1,7 @@
+"""
+Provide a container for dataset and fft operation on dataset.
+UI module can ask for a pair of x and y value for plotting.
+"""
 from enum import Enum, auto
 from fft_tools import *
 
@@ -10,6 +14,7 @@ class Axis(Enum):
 
 
 class FftData(Enum):
+    """FFT data enum for accessing dictionary from calculate_fft"""
     FREQ = auto()
     MAG = auto()
 
@@ -18,7 +23,7 @@ class Dataset:
     """
     Class for storing measurement data in 3 axes and perform FFT operation on it
     """
-    def __init__(self, data_sampling_period: float = 0.001, file_name: str = None):
+    def __init__(self, data_sampling_period: float = 0.001, file_name: str = None) -> None:
         if file_name is not None:
             pass
 
@@ -28,6 +33,11 @@ class Dataset:
 
     @staticmethod
     def generate_sample_data(time_data: np.ndarray) -> dict:
+        """
+        Populate dataset with sample data for default view of apps
+        :param time_data: time series data
+        :return: dictionary of 3 axis sample data
+        """
         sample_data_dict = {
             Axis.X: np.sin(100 * np.pi * time_data) + 0.5 * np.random.randn(len(time_data)),
             Axis.Y: np.sin(350 * np.pi * time_data) + 0.5 * np.random.randn(len(time_data)),
@@ -36,23 +46,40 @@ class Dataset:
         return sample_data_dict
 
     def get_time_data(self) -> np.ndarray:
+        """
+        Get time series data from raw measurement data.
+        This time series is created based on measurement sampling period
+        :return: numpy array
+        """
         return self.time_data
 
     def get_measurement_raw_data(self, axis: Axis) -> np.ndarray:
+        """
+        Get single axis of measurement raw data
+        :param axis: data axis (e.g x, y, or z)
+        :return: numpy array
+        """
         return self.measurement_raw_data_dict[axis]
 
     def get_fft_data(self, axis: Axis, index_min: int = 0, index_max: int = -1) -> dict:
+        """
+        Calculate fft from dataset and returning it as dictionary of fft data.
+        :param axis: axis of dataset (e.g. x, y, z)
+        :param index_min: start of dataset for fft calculation
+        :param index_max: end of dataset for fft calculation
+        :return: dictionary of fft result magnitude and frequency
+        """
         selected_data = self.get_measurement_raw_data(axis)[index_min:index_max]
         result = calculate_fft(selected_data, self.data_sampling_period)
 
         return {key: data for key, data in zip(FftData, result)}
 
-    def update_data(self, filename: str, data_sampling_period: float):
+    def update_data(self, filename: str, data_sampling_period: float) -> None:
         """
-        Update data from csv file
-        :param filename:
-        :param data_sampling_period:
-        :return:
+        update internal dataset from a csv file
+        :param filename: fullpath to csv file
+        :param data_sampling_period: in seconds
+        :return: None
         """
         raw_data = get_3axis_raw_data(filename, remove_dc=True)
 
